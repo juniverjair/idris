2674bda841696c6c1d41cf7074425d83b7f9f6a3
juniverjair <jjroman@espol.edu.ec>
2018-07-16 01:59:36 -0500

    Camina

---
+++ Assets/scripts/jugadorControlador.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jugadorControlador : MonoBehaviour {
+    public float maxSpeed = 5f;
+    public float speed = 2f;
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        rb2d.AddForce(Vector2.right * speed * h);
+        if (rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+    }
+}
